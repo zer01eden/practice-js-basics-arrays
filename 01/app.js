09ae60acf0ad0f89a46bff0e57fdc0b2fd3e8a56
zero_jeden <zeroitjeden@gmail.com>
2024-06-07 19:03:10 +0200

    solution 1

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+users = [
+	'zero jeden',
+	'zero dwa',
+	'zero trzy',
+	'zero cztery',
+	'jeden zero ',
+	'jeden dwa',
+];
+
+console.log(users[0]);
+console.log(users[2]);
+console.log(users[4]);
+console.log(users.length);
