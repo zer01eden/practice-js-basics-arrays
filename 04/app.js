e343d24e226912cfc6269f7802e6429004f44138
zero_jeden <zeroitjeden@gmail.com>
2024-06-08 21:23:02 +0200

    save & clean

--- 04/app.js
+++ 04/app.js
@@ -1,9 +1,9 @@
 const years = [1980, 1934, 2002, 2019];
 
-yearGoneBy = years.map(yearGoneBy)
+yearGoneBy = years.map(yearGoneBy);
 
-function yearGoneBy (element) {
-    return 2024 - element
+function yearGoneBy(element) {
+	return 2024 - element;
 }
 
 console.log(yearGoneBy);
