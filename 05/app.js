336b985ed059633082621206f60fda7ed71d93da
zero_jeden <zeroitjeden@gmail.com>
2024-06-08 21:56:17 +0200

    solution 5

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,18 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+let numbersIsEven = [];
+let sumEven = 0;
+
+numbers.filter(isEven);
+console.log(numbersIsEven);
+sumEven = numbersIsEven.reduce(sumNumbers);
+console.log(sumEven);
+
+function isEven(element) {
+	if (element % 2 === 0) {
+		numbersIsEven.push(element);
+	}
+}
+
+function sumNumbers(element, num) {
+	return element + num;
+}
